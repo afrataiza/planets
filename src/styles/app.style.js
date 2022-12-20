14411cd1b7d5129c510e010208eafdcabff8811c
Afra Taiza <afrataiza@gmail.com>
2022-12-19 22:28:46 -0300

    cria estilização do title usando styled component

---
+++ src/styles/app.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.body`
+box-sizing: border-box;
+background-image: url(../images/background.png);
+`;
+
+export default StyledApp;
---
+++ src/styles/title.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyledTitle = styled.h2`
+background: rgba(24, 26, 37, 0.5);
+mix-blend-mode: hard-light;
+border: 1px solid #FFFFFF;
+border-radius: 17px;
+font-family: 'Epilogue';
+font-style: normal;
+font-weight: 500;
+font-size: 3rem;
+color: #FFFFFF;
+width: 80%;
+height: 98px;
+display: flex;
+justify-content: center;
+align-items: center
+`;
+
+export default StyledTitle;
